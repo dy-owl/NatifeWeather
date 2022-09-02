dba58d5eddb537f5d1c33bdbd5b5b5529db2a8ca
Dmytro Yantsybaiev <d.yantsybaiev@geniusee.com>
2022-09-02 16:59:33 +0300

    Initial Commit

---
+++ NatifeWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NatifeWeather
+//
+//  Created by Dmytro Yantsybaiev on 02.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
